e0f1a272d70248cdcb7f6ea00bf8497edc748f26
nisa-basalamah <73590231+nisa-basalamah@users.noreply.github.com>
2023-09-27 02:56:44 +0700

    fetch category and sub category from database

--- src/fetch/categories.js
+++ src/fetch/categories.js
@@ -1,9 +1,25 @@
 import BASE_URL from "@/lib/baseUrl";
 import { getCookie } from "cookies-next";
 
-export const getOneCategory = async (id) => {
-  const token = getCookie("accessToken");
+const token = getCookie("accessToken");
 
+export const getAllCategory = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/cms/category`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const { data } = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getOneCategory = async (id) => {
   try {
     const response = await fetch(`${BASE_URL}/cms/category/${id}`, {
       method: "GET",
