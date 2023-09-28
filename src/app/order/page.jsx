1a3952ae2cf2ec128d626e7a500e16ea621a3dc9
ling-ling30 <126201256+ling-ling30@users.noreply.github.com>
2023-09-28 14:25:16 +0700

    first commit

--- src/app/order/page.jsx
+++ src/app/order/page.jsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <div>Order</div>;
+import fetchWithTokenServer from "@/lib/fetchWithTokenServer";
+import Table from "./components/Table";
+import { getCookies } from "cookies-next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+
+export default async function Page() {
+  if (!cookies().get(`accessToken`)) {
+    redirect(`/login`);
+  }
+  // console.log("page server");
+  const res = await fetchWithTokenServer("cms/order", "GET");
+  if (res === "Unauthorized") {
+    redirect("/logout");
+  }
+  const data = res.data;
+  if (!data) return;
+  return <Table />;
 }
