b9d9a199d065a503761d2fa55450b4363f2cad17
nisa-basalamah <73590231+nisa-basalamah@users.noreply.github.com>
2023-10-14 02:02:19 +0700

    add response status check for uploading image

--- src/fetch/files.js
+++ src/fetch/files.js
@@ -6,6 +6,11 @@ export const uploadFiles = async (formData) => {
       method: "POST",
       body: formData,
     });
+    
+    if (response.status >= 400) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
     const data = await response.json();
 
     return data;
